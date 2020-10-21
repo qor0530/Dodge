14042694ba8ebf46df39d97f9f9ae69a59fb4340
qor0530 <qorwhdghk12@gmail.com>
2020-10-21 18:31:07 +0900

    완성
    1. Text 구현
    2. 게임 매니저 구현

--- Dodge/Assets/Scripts/PlayerController.cs
+++ Dodge/Assets/Scripts/PlayerController.cs
@@ -46,5 +46,7 @@ public class PlayerController : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
     }
 }
